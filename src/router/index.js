332e4da6e45bf55c0bfafd86b9895ce89ce930c0
zlk <123434@qq.com>
2023-03-07 18:22:36 +0800

    zheng5

--- src/router/index.js
+++ src/router/index.js
@@ -21,6 +21,8 @@ import importExcel from "@/components/UploadExcel"
 import socialhis from "@/views/social/Historical";
 import reportforms from "@/views/social/ReportForms";
 import setup from "@/views/salarys/SetUp"
+import viewSee from "@/views/approvals/viewSee"
+import attimport from "@/views/attendances"
 Vue.use(VueRouter);
 const routes = [
   // 登录
@@ -36,23 +38,23 @@ const routes = [
       {
         path: "dashboard",
         component: dashboard,
-        meta:{
-            title:"首页"
-         }
+        meta: {
+          title: "首页"
+        }
       },
       {
         path: "departments",
         component: departments,
-        meta:{
-          title:"组织架构"
-       }
+        meta: {
+          title: "组织架构"
+        }
       },
       {
         path: "employees",
         component: employees,
-        meta:{
-          title:"员工"
-       }
+        meta: {
+          title: "员工"
+        }
       },
       // 员工-查看
       {
@@ -62,102 +64,111 @@ const routes = [
       {
         path: "setting",
         component: setting,
-        meta:{
-          title:"公司设置"
-       }
+        meta: {
+          title: "公司设置"
+        }
       },
       {
         path: "permission",
         component: permission,
-        meta:{
-          title:"权限设置"
-       }
+        meta: {
+          title: "权限设置"
+        }
       },
       {
         path: "social",
         component: social,
-        meta:{
-          title:"社保"
-       }
+        meta: {
+          title: "社保"
+        }
       },
       // 社保-历史归档
       {
-        path:"socialhis",
-        component:socialhis
+        path: "socialhis",
+        component: socialhis
       },
       // 社保-报表
       {
-        path:"reportforms",
-        component:reportforms
+        path: "reportforms",
+        component: reportforms
       },
       {
         path: "attendances",
         component: attendances,
-        meta:{
-          title:"考勤"
-       }
+        meta: {
+          title: "考勤"
+        }
       },
       {
         //考勤-月份报表
         path: "report/:month",
-          component: report,
-            meta: {
+        component: report,
+        meta: {
           title: "月份报表",
-              },
+        },
       },
       {
         //考勤- 历史归档
         path: "archiving",
-          component: archiving,
-            meta: {
+        component: archiving,
+        meta: {
           title: "历史归档",
-              },
+        },
       },
       {
-        path:"salarys",
-        component:salarys,
-        meta:{
-          title:'工资'
+        path: "salarys",
+        component: salarys,
+        meta: {
+          title: '工资'
         }
       },
       // 工资-设置
       {
-        path:"setup",
-        component:setup
+        path: "setup",
+        component: setup
       },
       {
-        path:"approvals",
-        component:approvals,
-        meta:{
-          title:'审批'
+        path: "approvals",
+        component: approvals,
+        meta: {
+          title: '审批'
         }
       },
       // 审批-流程设置
       {
-        path:"security",
-        component:security,
+        path: "security",
+        component: security,
       },
-            // 个人信息
-            {
-              path: "myInfo",
-              component:myInfo,
-              name: 'myInfo',
-              meta: {
-                title: '我的信息'
-              }
-            },
-           {
-            path:"falses",
-            component:falses,
-            meta:{
-              title:"404"
-            }
-           },
-          //  上传
-          {
-            path:"importExcel",
-            component:importExcel
-          }
+      {
+        // 审批
+        path: "viewSee/:processId",
+        component: viewSee,
+      },
+      // 个人信息
+      {
+        path: "myInfo",
+        component: myInfo,
+        name: 'myInfo',
+        meta: {
+          title: '我的信息'
+        }
+      },
+      {
+        path: "attimport",
+        component: attimport
+      },
+      {
+        path: "falses",
+        component: falses,
+        meta: {
+          title: "404"
+        }
+      },
+      //  上传
+      {
+        path: "importExcel",
+        component: importExcel
+      }
     ],
   },
 
